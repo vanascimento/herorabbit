79c23473e6527f7bd08675f1aa32212cc41e8eec
Victor Nascimento <victooyooth@gmail.com>
2025-04-01 07:03:58 -0300

    docs

---
+++ documentation/app/layout.tsx
@@ -0,0 +1,60 @@
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+import Link from "next/link";
+
+export const metadata = {
+  // Define your metadata here
+  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+};
+
+const banner = <Banner storageKey="some-key">Project under development</Banner>;
+const navbar = (
+  <Navbar
+    logo={
+      <>
+        <img
+          src="/images/general/rabbitmq-logo.svg"
+          alt="Logo"
+          width={25}
+          height={20}
+        />
+        <span style={{ marginLeft: ".4em", fontWeight: 800 }}>Hero Rabbit</span>
+      </>
+    }
+  />
+);
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
+
+export default async function RootLayout({ children }) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        <link rel="shortcut icon" href="/images/general/rabbitmq-logo.svg" />
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <Layout
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/vanascimento/herorabbit/tree/main"
+          footer={footer}
+          // ... Your additional layout options
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  );
+}
