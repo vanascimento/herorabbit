6a634a5e55dd4969e153d763ece4227229a20bb5
Victor Nascimento <victooyooth@gmail.com>
2025-05-06 21:52:50 -0300

    refatorar: reorganiza a estrutura do componente QueueBarOverviewChart para melhorar a legibilidade e a manutenção do código

--- extension/src/components/content/queue-and-streams/queue-bar-chart.tsx
+++ extension/src/components/content/queue-and-streams/queue-bar-chart.tsx
@@ -28,51 +28,49 @@ export function QueueBarOverviewChart() {
   ];
 
   return (
-    <div className="ext-relative ext-w-full ext-h-full ext-p-4">
-      <Card className="ext-flex ext-flex-col ext-w-1/2 ext-m-2 ext-rounded-sm">
-        <CardHeader>
-          <div className="ext-flex ext-flex-row ext-justify-between">
-            <div>
-              <CardTitle>{t('pages.queue.chart.title')}</CardTitle>
-              <CardDescription>
-                {t('pages.queue.chart.description', {
-                  top: topItems,
-                })}
-              </CardDescription>
-            </div>
-            <div className="ext-flex ext-flex-row ext-self-end ext-space-x-4">
-              <Input
-                placeholder={t('pages.queue.chart.filter')}
-                value={queueNameFilter}
-                onChange={(value) => setQueueNameFilter(value.target.value)}
-                onWheel={(e) => e.preventDefault()}
-                className="ext-text-center ext-rounded-sm"
-                type="text"
-              />
-              <Input
-                onChange={(value) => setTopItems(Number(value.target.value))}
-                onWheel={(e) => e.preventDefault()}
-                className="ext-text-center ext-rounded-sm"
-                value={topItems}
-                type="number"
-              />
-            </div>
+    <Card className="ext-flex ext-flex-col ext-w-1/2  ext-rounded-sm">
+      <CardHeader>
+        <div className="ext-flex ext-flex-row ext-justify-between">
+          <div>
+            <CardTitle>{t('pages.queue.chart.title')}</CardTitle>
+            <CardDescription>
+              {t('pages.queue.chart.description', {
+                top: topItems,
+              })}
+            </CardDescription>
           </div>
-        </CardHeader>
-        <CardContent className="ext-flex-1 ext-pb-0">
-          <ChartContainer config={{}} className="ext-aspect-square ext-max-h-[500px]">
-            <BarChart accessibilityLayer data={allQueues} layout="vertical" margin={{ right: 16 }}>
-              <YAxis dataKey="name" type="category" tickLine={false} tickMargin={10} axisLine={false} hide />
-              <XAxis dataKey="messages" type="number" hide />
-              <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="line" />} />
-              <Bar dataKey="messages" layout="vertical" radius={4}>
-                <LabelList dataKey="name" position="center" className="ext-fill-white ext-font-semibold" offset={8} />
-                <LabelList dataKey="messages" position="right" offset={8} fontSize={12} />
-              </Bar>
-            </BarChart>
-          </ChartContainer>
-        </CardContent>
-      </Card>
-    </div>
+          <div className="ext-flex ext-flex-row ext-self-end ext-space-x-4">
+            <Input
+              placeholder={t('pages.queue.chart.filter')}
+              value={queueNameFilter}
+              onChange={(value) => setQueueNameFilter(value.target.value)}
+              onWheel={(e) => e.preventDefault()}
+              className="ext-text-center ext-rounded-sm"
+              type="text"
+            />
+            <Input
+              onChange={(value) => setTopItems(Number(value.target.value))}
+              onWheel={(e) => e.preventDefault()}
+              className="ext-text-center ext-rounded-sm"
+              value={topItems}
+              type="number"
+            />
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="ext-flex-1 ext-pb-0">
+        <ChartContainer config={{}} className="ext-aspect-square ext-max-h-[500px]">
+          <BarChart accessibilityLayer data={allQueues} layout="vertical" margin={{ right: 16 }}>
+            <YAxis dataKey="name" type="category" tickLine={false} tickMargin={10} axisLine={false} hide />
+            <XAxis dataKey="messages" type="number" hide />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="line" />} />
+            <Bar dataKey="messages" layout="vertical" radius={4}>
+              <LabelList dataKey="name" position="center" className="ext-fill-white ext-font-semibold" offset={8} />
+              <LabelList dataKey="messages" position="right" offset={8} fontSize={12} />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
   );
 }
